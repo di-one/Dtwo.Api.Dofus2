c78db70c88eaa0207ed9252b8fd7ee22e963cc2b
Flutes <Flutes@DESKTOP-9I523O8>
2024-05-13 23:02:17 +0200

    edit SupressMessage scope

--- SupressMessagesAsm.cs
+++ SupressMessagesAsm.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Compiler", "CS8618", Target = "N:Dtwo.API.Dofus2.AnkamaGames.Dofus.DataCenter", Scope = "namespaceanddescendants")]
-[assembly: SuppressMessage("Compiler", "CS8618", Target = "N:Dtwo.API.Dofus2.AnkamaGames.Network.Messages", Scope = "namespaceanddescendants")]
+[assembly: SuppressMessage("Compiler", "CS8618", Target = "N:Dtwo.API.Dofus2.AnkamaGames.Dofus", Scope = "namespaceanddescendants")]
+[assembly: SuppressMessage("Compiler", "CS8618", Target = "N:Dtwo.API.Dofus2.AnkamaGames.Network", Scope = "namespaceanddescendants")]
 
 namespace Dtwo.API.Dofus2
 {
