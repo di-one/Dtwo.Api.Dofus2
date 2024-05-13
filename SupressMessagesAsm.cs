5aa13cc7955b03895c6cb0e40b5708e0de49738a
Flutes <Flutes@DESKTOP-9I523O8>
2024-05-13 19:52:06 +0200

    Add SuppressMessage attributes for disable some warnings in Dofus2 classes (auto generated)

---
+++ SupressMessagesAsm.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Compiler", "CS8618", Target = "N:Dtwo.API.Dofus2.AnkamaGames.Dofus.DataCenter", Scope = "namespaceanddescendants")]
+[assembly: SuppressMessage("Compiler", "CS8618", Target = "N:Dtwo.API.Dofus2.AnkamaGames.Network.Messages", Scope = "namespaceanddescendants")]
+
+namespace Dtwo.API.Dofus2
+{
+
+}
